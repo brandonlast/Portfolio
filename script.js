3bf3bb6a1a165c1dcd8320000b0eeda60f8cc930
brandonlast <133057528+brandonlast@users.noreply.github.com>
2024-04-10 17:23:49 -0400

    Added header, Hero, and updated footer

---
+++ script.js
@@ -0,0 +1,26 @@
+let text = "Network Administrator...";
+        let speed = 100;
+        let elem = document.getElementById('cycle');
+        
+        function typeWriter() {
+            if (elem.textContent.length > 0) {
+                elem.textContent = elem.textContent.slice(0, -1);
+                setTimeout(typeWriter, speed);
+            } else {
+                typeMessage(text, 0);
+            }
+        }
+
+        function typeMessage(message, index) {
+            if (index < message.length) {
+                elem.textContent += message.charAt(index);
+                index++;
+                setTimeout(function() {
+                    typeMessage(message, index);
+                }, speed);
+            }
+        }
+
+        setTimeout(function(){
+            typeWriter();
+        }, 3000);
