509d5587c01a6865addc43d1f51fad805c281ddd
brandonlast <133057528+brandonlast@users.noreply.github.com>
2024-04-12 23:48:14 -0400

    fixed spacing from email response

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,8 @@ btn.addEventListener('click',function(e){
     var email = document.getElementById('email').value;
     var subject = document.getElementById('subject').value;
     var message = document.getElementById('message').value;
-    var body = 'name: ' +name + '<br/> email: ' + email + '<br/> subject' +
-    subject + '<br/> message' +message; 
+    var body = 'name: ' +name + '<br/> email: ' + email + '<br/> subject: ' +
+    subject + '<br/> message: ' + message; 
 
 
     Email.send({
