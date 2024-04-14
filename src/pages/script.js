7862b05a58cb29f1e17c7a2af896b9843b7b00bc
brandonlast <133057528+brandonlast@users.noreply.github.com>
2024-04-14 18:39:59 -0400

    Moved Darktheme JS into its own file

--- src/pages/script.js
+++ src/pages/script.js
@@ -28,24 +28,3 @@ setTimeout(function(){
 }, 3000);
 
 
-const toggleButton = document.getElementById('dark-theme-toggle');
-const body = document.body;
-
-function toggleTheme() {
-  const isDarkTheme = body.classList.contains('dark-theme');
-  body.classList.toggle('dark-theme');
-
-  const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('navbar-dark', isDarkTheme);
-
-  localStorage.setItem('theme', isDarkTheme ? 'light' : 'dark');
-}
-
-toggleButton.addEventListener('click', toggleTheme);
-
-const themePreference = localStorage.getItem('theme');
-if (themePreference === 'dark') {
-  body.classList.add('dark-theme');
-}
-
-
